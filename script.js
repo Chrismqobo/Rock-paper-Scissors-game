af3d76c7f0dcd9dc444c19e9ab73c768cf38bee9
Chrisnerg <chrisnergmqobo@gmail.com>
2025-02-25 17:52:38 +0200

    Updated the function call RoundDecider() inside PlayRound() function by passing the 2 parameters

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 /** Variables */
 let PlayerChoice = null;
 let ComputerChoice = null;
-let PlayerScore = null;
-let ComputerScore = null;
+let PlayerScore = 0;
+let ComputerScore = 0;
 let randNum = null;
 let GameStatus = null;
 
@@ -15,10 +15,6 @@ let GameStatus = null;
     /**Function for picking the computer choice*/
     function getComputerChoice()
     {
-        if(getrandomNumber(3) === null)
-        {
-            console.log("Computer choice not captured");
-        }
 
         randNum = getrandomNumber(3);
         if(randNum === 0)
@@ -42,7 +38,7 @@ let GameStatus = null;
     /**Validating the player choice */
     function CheckPlayerChoice(choice)
     {
-        if(PlayerChoice === "rock" || PlayerChoice === "paper" || PlayerChoice === "scissos")
+        if(PlayerChoice === "rock" || PlayerChoice === "paper" || PlayerChoice === "scissors")
         {
             return true;
         }
@@ -72,60 +68,62 @@ let GameStatus = null;
     /**Checking which player won the round */
    function RoundDecider(PlayerChoice,ComputerChoice)
    {
+        
         if(ComputerChoice === "rock" && PlayerChoice === "rock")
         {
-            console.log("*** Computer: Rock - Player: Rock *** \n");
+            console.log("\n *** Computer: Rock - Player: Rock *** \n");
             console.log("******* DRAW *******");  
         }
         else if(ComputerChoice === "rock" && PlayerChoice === "paper")
         {
-            console.log("*** Computer: Rock - Player: Paper *** \n");
+            console.log("\n *** Computer: Rock - Player: Paper *** \n");
             console.log("******* YOU WIN *******");
             PlayerScore += 1;
         }
         else if(ComputerChoice === "rock" && PlayerChoice === "scissors")
         {
-            console.log("*** Computer: Rock - Player: Scissors *** \n");
+            console.log("\n *** Computer: Rock - Player: Scissors *** \n");
             console.log("******* YOU LOOSE *******");
             ComputerScore += 1;
         }
         else if(ComputerChoice === "paper" && PlayerChoice === "paper")
         {
-            console.log("*** Computer: Paper - Player: Paper *** \n");
+            console.log("\n *** Computer: Paper - Player: Paper *** \n");
             console.log("******* DRAW *******");
         }
         else if(ComputerChoice === "paper" && PlayerChoice === "rock")
         {
-            console.log("*** Computer: paper - Player: rock *** \n");
+            console.log("\n *** Computer: paper - Player: rock *** \n");
             console.log("******* YOU LOOSE *******");
             ComputerScore += 1;
         }
         else if(ComputerChoice === "paper" && PlayerChoice === "scissors")
         {
-            console.log("*** Computer: paper - Player: scissors *** \n");
+            console.log("\n *** Computer: paper - Player: scissors *** \n");
             console.log("******* YOU WIN *******");
             PlayerScore += 1;
         }
         else if(ComputerChoice === "scissors" && PlayerChoice === "scissors")
         {
-            console.log("*** Computer: scissors - Player: scissors *** \n");
+            console.log("\n *** Computer: scissors - Player: scissors *** \n");
             console.log("******* DRAW *******");
         }
         else if(ComputerChoice === "scissors" && PlayerChoice === "paper")
         {
-            console.log("*** Computer: scissors - Player: paper *** \n");
+            console.log("\n *** Computer: scissors - Player: paper *** \n");
             console.log("******* YOU LOOSE *******");
             ComputerScore += 1;
         }
         else if(ComputerChoice === "scissors" && PlayerChoice === "rock")
         {
-            console.log("*** Computer: scissors - Player: rock *** \n");
+            console.log("\n *** Computer: scissors - Player: rock *** \n");
             console.log("******* YOU WIN *******");
             PlayerScore += 1;
         }
    }
 
-   /**Checking the winner for the game */
+   
+   /**Checking the winner for the game*/
    function DecideWinner()
    {
     if(PlayerScore > ComputerScore)
@@ -147,7 +145,7 @@ let GameStatus = null;
    {
     getComputerChoice();
     getPlayerChoice();
-    RoundDecider();
+    RoundDecider(getPlayerChoice(),getComputerChoice());
    }
 
    function PlayGame()
@@ -155,8 +153,11 @@ let GameStatus = null;
     console.log("**** Welcome Player **** \n");
     for(let i = 0; i < 5; i++)
     {
+        console.log(`\n **** ROUND ${i + 1} ****\n`)
         PlayRound();
     }
+    DecideWinner();
    }
 
-   PlayGame();
+   console.log(PlayGame());
+   
